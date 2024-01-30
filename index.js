57f499177b74c3dee4f8d4ae121de70dfca80ad1
Habib-0007 <adebayohabib7@gmail.com>
2024-01-30 18:09:12 +0100

    new change to the JS file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-let pickupBtn = document.querySelector("#getPickup");
-let pickupContent = document.querySelector(".pickupContent");
+var pickupBtn = document.querySelector("#getPickup");
+var pickupContent = document.querySelector(".pickupContent");
 
 pickupBtn.addEventListener("click", () => {
-    fetch("pickups.json")
-    .then( res => res.json())
-    .then( data => { 	let random = Math.floor(Math.random() * data.length);
-	let randomPickup = data[random].pickup;
-	pickupContent.innerText = randomPickup;
-       });
+	fetch("https://pickup-lines-api.onrender.com/data")
+		.then(res => res.json())
+		.then(data => {
+			let random = Math.floor(Math.random() * data.length);
+			let randomPickup = data[random].pickup;
+			pickupContent.textContent = randomPickup;
+		})
+		.catch(err => console.error(err));
 });
